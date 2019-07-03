e6c2abe6fef0d3e54304fc82945cb9a44732e5d5
matthewellishanson <lifeasmeh@gmail.com>
2019-07-02 22:59:06 -0500

    adding api key and base map layers

---
+++ static/js/logic.js
@@ -0,0 +1,13 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [41.8781, -87.6298],
+    zoom: 11
+});
+
+// Adding tile layer to the map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
