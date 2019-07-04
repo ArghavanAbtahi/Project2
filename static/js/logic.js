cc416f104b9183254873e1c61a462914769e3cc2
matthewellishanson <lifeasmeh@gmail.com>
2019-07-03 19:23:34 -0500

    removed extra tile layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -31,15 +31,6 @@ function createMap(airbnbMarkers) {
     collapsed: false
   }).addTo(map);
 }
-
-// // Adding tile layer to the map
-// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   maxZoom: 18,
-//   id: "mapbox.streets",
-//   accessToken: API_KEY
-// }).addTo(myMap);
-
 // add url for json stored in app route /airbnb
 var defaultURL = "/airbnb";
 
