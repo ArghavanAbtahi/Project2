02b898b70905c6be509ea3b4a7fa4dbf1245ad6e
matthewellishanson <lifeasmeh@gmail.com>
2019-07-03 22:20:06 -0500

    i < 55

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def test():
   print("preparing dictionary to jsonfiy")
   data = []
   i = 0
-  while i < len(df):
+  while i < 55:
     air = {
       'neighbourhood':list(df['neighbourhood'])[i],
       'neighborhood_overview':list(df['neighborhood_overview'])[i],
