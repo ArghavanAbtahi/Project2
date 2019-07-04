4b2bc9563116ea883a133eb9d9d10ad9c32798d8
matthewellishanson <lifeasmeh@gmail.com>
2019-07-03 19:37:15 -0500

    switching to light map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,16 @@
 function createMap(airbnbListings) {
 
   // Create the tile layer that will be the background of our map
-  var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox.light",
     accessToken: API_KEY
   });
 
   // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
-    "Street Map": streetMap
+    "Light Map": lightmap
   };
 
   // Create an overlayMaps object to hold the bikeStations layer
@@ -21,9 +21,9 @@ function createMap(airbnbListings) {
   // Create the map object with options
   var map = L.map("map", {
     center: [41.8781, -87.6298],
-    zoom: 12
+    zoom: 12,
+    layers: [lightmap, airbnbListings]
   });
-
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
