a42f1d8c704c12460758435fe7b8028662e97397
matthewellishanson <lifeasmeh@gmail.com>
2019-07-06 10:03:13 -0500

    limit to numelementsremaining

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def test():
   print("*** Preparing to convert df with " + str(numElementsRemaining) + " elements to a dictionary")
   data = []
   i = 0
-  while i < 500:
+  while i < numElementsRemaining:
     air = {
       'neighbourhood':list(df['neighbourhood'])[i],
       'latitude':list(df['latitude'])[i],
