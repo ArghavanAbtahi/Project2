2945a6d3066c60d376d84e90296c1a784ab627ac
matthewellishanson <lifeasmeh@gmail.com>
2019-07-05 22:32:31 -0500

    extracting d3 scatter code out of for loop, rm loop

--- static/js/app.js
+++ static/js/app.js
@@ -34,74 +34,72 @@ d3.json(defaultURL, function(error, response) {
     var numProperties = Object.keys(response).length;
     console.log("*** Number of properties received: " + numProperties);
 
-    for (var i = 0; i < response.length; i++) {
-        airbnbProperty = response[i]
-        response.forEach(function(airbnbProperty) {
-            airbnbProperty.accommodates = +airbnbProperty.accommodates;
-            airbnbProperty.price = +airbnbProperty.price;
-            airbnbProperty.rating = +airbnbProperty.rating;
+    response.forEach(function(i) {
+        i.accommodates = +i.accommodates;
+        i.price = +i.price;
+        i.rating = +i.rating;
+    });
+    console.log(response[0]);
+    console.log("*** Highest price received: " + d3.max(response, i => i.price));
+    
+    // Create scale functions
+    var xScatterScale = d3.scaleLinear()
+    .domain([0, d3.max(response, i => i.price)])
+    .range([0, scatterWidth]);
+
+    var yScatterScale = d3.scaleLinear()
+        .domain([0, d3.max(response, i => i.accommodates)])
+        .range([scatterHeight, 0]);
+
+    // Create axis functions
+    var scatterBottomAxis = d3.axisBottom(xScatterScale);
+    var scatterLeftAxis = d3.axisLeft(yScatterScale);
+
+    scatterGroup.append("g").attr("transform", `translate(0, ${scatterHeight})`).call(scatterBottomAxis);
+    scatterGroup.append("g").call(scatterLeftAxis);
+
+        // Create circles
+    var circles = scatterGroup.selectAll("circle")
+    .data(response)
+    .enter()
+    .append("circle")
+    .attr("cx", i => xScatterScale(i.price))
+    .attr("cy", i => yScatterScale(i.accommodates))
+    .attr("r", "10")
+    .attr("fill", "salmon")
+    .attr("opacity", ".7");
+
+    // Initialize tooltip
+    var scatterTooltip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([80, -60])
+    .html(function(i) {
+        return (`${i.property_type}<br>Price: ${i.price}<br> Capacity: ${i.accommodates}`);
+    });
+
+    // Call tooltip
+    scatterGroup.call(scatterTooltip);
+
+    // Display and hide the tooltip on hover
+    circles.on("mouseover", function(response) {
+        scatterTooltip.show(response, this);
+    })
+        // hover
+        .on("mouseout", function(response, index) {
+        scatterTooltip.hide(response);
         });
-        console.log(response[0]);
-        console.log("*** Highest price received: " + d3.max(response, airbnbProperty => airbnbProperty.price));
+
+    scatterGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 10 - scatterMargin.left + 40)
+    .attr("x", 10 - (scatterHeight / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Property capacity");
+
+    scatterGroup.append("text")
+    .attr("transform", `translate(${scatterWidth / 2}, ${scatterHeight + scatterMargin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Rental price");
     
-        // Create scale functions
-        var xScatterScale = d3.scaleLinear()
-        .domain([0, d3.max(response, airbnbProperty => airbnbProperty.price)])
-        .range([0, scatterWidth]);
-    
-        var yScatterScale = d3.scaleLinear()
-            .domain([0, d3.max(response, airbnbProperty => airbnbProperty.accommodates)])
-            .range([scatterHeight, 0]);
-    
-        // Create axis functions
-        var scatterBottomAxis = d3.axisBottom(xScatterScale);
-        var scatterLeftAxis = d3.axisLeft(yScatterScale);
-    
-        scatterGroup.append("g").attr("transform", `translate(0, ${scatterHeight})`).call(scatterBottomAxis);
-        scatterGroup.append("g").call(scatterLeftAxis);
-
-         // Create circles
-        var circles = scatterGroup.selectAll("circle")
-        .data(response)
-        .enter()
-        .append("circle")
-        .attr("cx", airbnbProperty => xScatterScale(airbnbProperty.price))
-        .attr("cy", airbnbProperty => yScatterScale(airbnbProperty.accommodates))
-        .attr("r", "10")
-        .attr("fill", "salmon")
-        .attr("opacity", ".7");
-
-        // Initialize tooltip
-        var scatterTooltip = d3.tip()
-        .attr("class", "d3-tip")
-        .offset([80, -60])
-        .html(function(airbnbProperty) {
-            return (`${airbnbProperty.property_type}<br>Price: ${airbnbProperty.price}<br> Capacity: ${airbnbProperty.accommodates}`);
-        });
-
-        // Call tooltip
-        scatterGroup.call(scatterTooltip);
-
-        // Display and hide the tooltip on hover
-        circles.on("mouseover", function(response) {
-            scatterTooltip.show(response, this);
-        })
-            // hover
-            .on("mouseout", function(response, airbnbProperty) {
-            scatterTooltip.hide(response);
-            });
-
-        scatterGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 10 - scatterMargin.left + 40)
-        .attr("x", 10 - (scatterHeight / 2))
-        .attr("dy", "1em")
-        .attr("class", "axisText")
-        .text("Property capacity");
-
-        scatterGroup.append("text")
-        .attr("transform", `translate(${scatterWidth / 2}, ${scatterHeight + scatterMargin.top + 30})`)
-        .attr("class", "axisText")
-        .text("Rental price");
-        }
 });
